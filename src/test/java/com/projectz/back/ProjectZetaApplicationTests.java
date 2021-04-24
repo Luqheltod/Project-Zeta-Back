8fd856753d948bf68bc40187eb9ebc9166203111
German Sanchez Pastor <german.sanchezpastor@innotec.security>
2021-04-24 20:24:41 +0200

    Iniciando repositorio y primera configuracion jdbc

---
+++ src/test/java/com/projectz/back/ProjectZetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectz.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectZetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
