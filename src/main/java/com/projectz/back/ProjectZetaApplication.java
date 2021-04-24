63220b4f8ce622a959bd4cf90f9d181b1ba620e5
German Sanchez <Luqh555@gmail.com>
2021-04-24 20:53:28 +0200

    git ignore working

--- src/main/java/com/projectz/back/ProjectZetaApplication.java
+++ src/main/java/com/projectz/back/ProjectZetaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectZetaApplication {
 
 	public static void main(String[] args) {
-		SpringApplicationa.run(ProjectZetaApplication.class, args);
+		SpringApplication.run(ProjectZetaApplication.class, args);
 	}
 
 }
