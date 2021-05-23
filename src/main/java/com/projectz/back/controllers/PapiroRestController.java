249a4b25db934f7932ddf372b7b1113416148afc
German Sanchez <Luqh555@gmail.com>
2021-05-23 21:41:35 +0200

    comienzo del back configuracion y controladores

---
+++ src/main/java/com/projectz/back/controllers/PapiroRestController.java
@@ -0,0 +1,10 @@
+package com.projectz.back.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/papiro")
+public class PapiroRestController {
+
+}
