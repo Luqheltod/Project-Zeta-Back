fd29eadac0c9a0c96b6ad152b04fe1c0015523d5
German Sanchez <Luqh555@gmail.com>
2021-05-30 22:32:58 +0200

    Modificamos clase papiro

--- src/main/java/com/projectz/back/model/Papiro.java
+++ src/main/java/com/projectz/back/model/Papiro.java
@@ -2,4 +2,47 @@ package com.projectz.back.model;
 
 public class Papiro {
 
+	
+	private Integer id;
+	
+	private String texto;
+	
+	private Integer idOpciones;
+	
+	private Integer idMapa;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Integer getIdOpciones() {
+		return idOpciones;
+	}
+
+	public void setIdOpciones(Integer idOpciones) {
+		this.idOpciones = idOpciones;
+	}
+
+	public Integer getIdMapa() {
+		return idMapa;
+	}
+
+	public void setIdMapa(Integer idMapa) {
+		this.idMapa = idMapa;
+	}
+
+	
+	
 }
