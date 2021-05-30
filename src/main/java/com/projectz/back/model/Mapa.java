d2a49e52577ab0ec48950f8880401d09ca3e6bcc
German Sanchez <Luqh555@gmail.com>
2021-05-30 22:40:11 +0200

    Clase mapa

---
+++ src/main/java/com/projectz/back/model/Mapa.java
@@ -0,0 +1,37 @@
+package com.projectz.back.model;
+
+public class Mapa {
+
+	
+	private Integer id;
+	
+	private Integer piso;
+	
+	private Integer habitacion;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getPiso() {
+		return piso;
+	}
+
+	public void setPiso(Integer piso) {
+		this.piso = piso;
+	}
+
+	public Integer getHabitacion() {
+		return habitacion;
+	}
+
+	public void setHabitacion(Integer habitacion) {
+		this.habitacion = habitacion;
+	}
+	
+	
+}
