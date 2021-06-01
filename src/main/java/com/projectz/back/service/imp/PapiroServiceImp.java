a253849521be35358bb1e57249310d4d409455f5
German Sanchez <Luqh555@gmail.com>
2021-06-01 21:38:08 +0200

    Api rest funcionando, preparado para pasar los datos al front

--- src/main/java/com/projectz/back/service/imp/PapiroServiceImp.java
+++ src/main/java/com/projectz/back/service/imp/PapiroServiceImp.java
@@ -1,11 +1,13 @@
 package com.projectz.back.service.imp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.projectz.back.mapper.PapiroMapper;
 import com.projectz.back.model.Papiro;
 import com.projectz.back.service.PapiroService;
 
+@Service
 public class PapiroServiceImp implements PapiroService{
 
 	@Autowired
